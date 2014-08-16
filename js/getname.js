0fa3b1f1d3a5bfddbcb41bd96e51d83aead70ef0
reppard <reppardwalker@gmail.com>
2014-08-16 15:45:52 -0400

    Render crafting table top

--- js/getname.js
+++ js/getname.js
@@ -1,3 +1,7 @@
+//////////////////////
+//Random Name Builder
+/////////////////////
+
 var ent_array = ["Experience", "Orb", "Lead", "Knot", "Painting", "Arrow", "Snowball", "Ghast",
                  "Fireball", "Blaze", "Ender", "Pearl", "Eye", "Splash", "Potion", "Bottle",
                  "Enchanting", "Frame", "Wither", "Skull", "Primed", "TNT", "Falling", "Block",
@@ -23,3 +27,50 @@ button.onclick = function () {
   div.innerHTML = "";
   div.innerHTML = getName();
 }
+
+////////////////////////
+//Craft Table...crafter.
+////////////////////////
+
+//block matrixs'
+  var colors = {
+    0: 'black',
+    1: '#1F140A', //darkest-brown
+    2: '#DBB84D', //tan
+    3: '#D6AD33',//dark-tan
+    4: '#A37547',//light-brown
+    5: '#5C3D1F',//dark-brown
+    6: '#5C1F00'//red-brown
+  }
+  var craftTableTop = [
+    [0,0,0,0,0,6,6,6,6,6,6,0,0,0,0,0],
+    [0,2,2,1,4,4,4,4,4,4,4,4,1,2,3,0],
+    [0,2,1,4,4,4,4,4,4,4,4,4,4,1,3,0],
+    [0,1,4,5,5,5,5,5,5,5,5,5,5,4,1,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,4,4,5,5,5,5,5,5,5,5,5,5,4,4,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,4,4,5,5,5,5,5,5,5,5,5,5,4,4,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,4,4,5,4,4,5,4,4,5,4,4,5,4,4,0],
+    [0,1,4,5,5,5,5,5,5,5,5,5,5,4,1,0],
+    [0,2,1,4,4,4,4,4,4,4,4,4,4,1,2,0],
+    [0,2,2,1,4,4,4,4,4,4,4,4,1,2,2,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  ]
+////////////////////////////////////////
+
+var tableDiv = document.getElementById('craftTable');
+for(i=0;i<16;i++){
+  tableDiv.innerHTML = tableDiv.innerHTML + '<div class="tabRows"></div>';
+}
+for(i=0;i<16;i++){
+  var node = tableDiv.childNodes[i];
+  for(n=0;n<16;n++){
+    node.innerHTML = node.innerHTML + '<div class="cell" style="background:' + colors[craftTableTop[i][n]] + ';"></div>';
+  }
+}
+var w = document.getElementsByClassName('container')[0].offsetWidth;
+tableDiv.style.height = w + 'px';
